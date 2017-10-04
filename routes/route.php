91193543f8d5a04ba35ad60177190738e03108c0
Giovanni Cruz <giovanni.cruz97@hotmail.com>
2017-10-03 21:43:36 -0300

    New route.php doc block

--- routes/route.php
+++ routes/route.php
@@ -1,19 +1,17 @@
 <?php
 
-/*
- |
- | -----------------------------------------------------------------
- | Web routes
- | -----------------------------------------------------------------
- | This is where you register your application routes
- | E.g $router->map('METHOD', 'URI', 'Controller@method')
- | Where METHOD can be GET, POST, PUT, PATCH, DELETE
- | URI /user/dashboard, /account/logout etc
- | Controller method is the method defined inside App\Controllers\Controller::class
- |
- |
+/**
+ * Web Routes
+ *
+ * This is where you register your application routes
+ * E.g $router->map('METHOD', 'URI', 'Controller@method')
+ * Where METHOD can be GET, POST, PUT, PATCH, DELETE
+ * URI /user/dashboard, /account/logout etc
+ * Controller method is the method defined inside App\Controllers\Controller::class
  */
 
 $router->map('GET', '/[a:name]', 'Controller@hello');
 $router->map('GET', '/', 'Controller@hello');
 $router->map('POST', '/', 'Controller@postTest');
+
+
