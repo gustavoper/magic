da6dba39bd118271f36c3af5f478f378e32f4409
Gustavo Pereira <gustavoper@gmail.com>
2017-10-07 22:52:56 -0300

    Added a few improvements in error class.

--- core/Router.php
+++ core/Router.php
@@ -1,57 +1,88 @@
 <?php
 
-namespace Core;
+    namespace Core;
 
-class Router
-{
-    public function __construct($req, $res) 
+    /**
+     * Class Router
+     *
+     * @access  public
+     * @package Core
+     */
+    class Router
     {
-        $match = $this->matchMaker();
+        private $request;
+        private $response;
 
-        if ($match == false) {
-			$this->routeError($res, 404);
-		}
-		else {
-            list($controller, $action) = explode('@', $match['target']);
-            $obj = $this->controllerMaker($controller);            
-            if (is_callable(array($obj, $action))) {
-                $obj->{$action}($req, $res, $match['params']);
-            } else {
-                $this->routeError($res, 500);
-            }
-        }
-    }
-
-    public function routeError($res, $code)
-    {
-        $res->headers->set('Content-Type', 'application/json');
-        
-        if ($code == 404)
+        /**
+         * Router constructor.
+         *
+         * @access
+         * @param Request $req
+         * @param Response $res
+         */
+        public function __construct($req, $res)
         {
-            $message = 'Route not found';
-        } else {
-            $message = 'Ops, you don\'t have mana'; 
+            $this->request = $req;
+            $this->response = $res;
+            $this->handleRouting();
         }
-        $res->setStatusCode($code);
-        $res->setContent(
-            json_encode([
-                'type' => 'error',
-                'message' => $message
-            ])
-        );
-        return $res->send();
-    }
 
-    public function matchMaker()
-    {
-        $router = new \AltoRouter();
-        include '../routes/route.php';
-        return $router->match();
-    }
+        /**
+         * Handle the routing/error responses
+         * @access public
+         * @return bool
+         */
+        private function handleRouting() : bool
+        {
+            $match = $this->matchMaker();
+            if ($match == false) {
+                $this->routeError($this->response, 404);
+                return false;
+            }
+            list($controller, $action) = explode('@', $match['target']);
+            $obj = $this->controllerMaker($controller);
+            if ($obj === null) {
+                $this->routeError($this->response, 500);
+                return false;
+            }
+            if (!is_callable(array($obj, $action))) {
+                $this->routeError(500);
+                return false;
+            }
+            $obj->{$action}($this->request, $this->response, $match['params']);
+            return true;
+        }
 
-    public function controllerMaker($controller)
-    {
-        $controller = 'App\\Controllers\\'.$controller;
-        return new $controller;
+        public function routeError($code)
+        {
+            $this->request->headers->set('Content-Type', 'application/json');
+            $message = 'Ops, you don\'t have mana';
+            if ($code == 404) {
+                $message = 'Route not found';
+            }
+            $this->request->setStatusCode($code);
+            $this->request->setContent(
+                json_encode([
+                    'type'    => 'error',
+                    'message' => $message
+                ])
+            );
+            return $this->request->send();
+        }
+
+        public function matchMaker()
+        {
+            $router = new \AltoRouter();
+            include '../routes/route.php';
+            return $router->match();
+        }
+
+        public function controllerMaker($controller)
+        {
+            $controller = 'App\\Controllers\\' . $controller;
+            if (!class_exists($controller)) {
+                return null;
+            }
+            return new $controller;
+        }
     }
-}
